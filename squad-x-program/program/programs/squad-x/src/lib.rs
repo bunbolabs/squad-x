873060a6f70b755b2ecca7e7af9088613e5c6932
s0nhaaa <nguyensonha.hanz@gmail.com>
2024-01-24 23:11:06 +0700

    fix: program

---
+++ squad-x-program/program/programs/squad-x/src/lib.rs
@@ -0,0 +1,122 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9d8JDdyrY5GW8epND5r3SQovSJP6SxWrajvpPYPB3Mhi");
+
+#[program]
+mod squad_x {
+    use super::*;
+
+    pub fn create_user_account(
+        ctx: Context<CreateUserAccount>,
+        display_name: String,
+        badge: String,
+    ) -> Result<()> {
+        let user = &mut ctx.accounts.user;
+        user.display_name = display_name;
+        user.level = 1;
+        user.badge = badge;
+
+        Ok(())
+    }
+
+    pub fn join_squad(ctx: Context<JoinSquad>) -> Result<()> {
+        let user = &mut ctx.accounts.user;
+        let squad = &mut ctx.accounts.squad;
+
+        require!(
+            ctx.accounts.system_program.key() != user.squad.key(),
+            Error::AlreadyJoinedSquad
+        );
+
+        user.squad = squad.key();
+        squad.members = squad.members + 1;
+
+        Ok(())
+    }
+
+    pub fn create_squad(
+        ctx: Context<CreateSquadAccount>,
+        display_name: String,
+        motto: String,
+        badge: String,
+    ) -> Result<()> {
+        let squad = &mut ctx.accounts.squad;
+        squad.display_name = display_name;
+        squad.motto = motto;
+        squad.badge = badge;
+        squad.level = 1;
+        squad.members = 1;
+        squad.owner = ctx.accounts.signer.key();
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct CreateUserAccount<'info> {
+    #[account(
+        init,
+        payer = signer,
+        seeds = [b"USERS", signer.key().as_ref()],
+        bump,
+        space = 8 + 256 + 2 + 256 + 32
+    )]
+    pub user: Account<'info, Users>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct JoinSquad<'info> {
+    #[account(mut)]
+    pub user: Account<'info, Users>,
+
+    #[account(mut)]
+    pub squad: Account<'info, Squads>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateSquadAccount<'info> {
+    #[account(
+        init,
+        payer = signer,
+        seeds = [b"SQUADS", signer.key().as_ref()],
+        bump,
+        space = 8 + 256 + 256 + 1 + 1 + 256 + 32,
+    )]
+    pub squad: Account<'info, Squads>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Users {
+    pub display_name: String,
+    pub level: u8,
+    pub badge: String,
+    pub squad: Pubkey,
+}
+
+#[account]
+pub struct Squads {
+    pub display_name: String,
+    pub motto: String,
+    pub level: u8,
+    pub members: u8,
+    pub badge: String,
+    pub owner: Pubkey,
+}
+
+#[error_code]
+pub enum Error {
+    #[msg("Already joined squad!")]
+    AlreadyJoinedSquad,
+}
--- squad-x-program/program/src/mod.rs
+++
@@ -1,5 +0,0 @@
-// This file is auto-generated from the CIDL source.
-// Editing this file directly is not recommended as it may be overwritten.
-
-pub mod create_user_account;
-pub mod create_squad_account;
